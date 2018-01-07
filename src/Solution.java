ce220a7a10af45f9c590ddd67d922ca0a73f5ec7
Arvis Abromovics <info@vsmedio.lv>
2018-01-08 00:09:31 +0100

    Initial commit

---
+++ src/Solution.java
@@ -0,0 +1,30 @@
+import java.util.*;
+
+/*
+LinkedList и ArrayList
+Нужно создать два списка - LinkedList и ArrayList.
+
+
+Требования:
+1. Программа не должна выводить текст на экран.
+2. Программа не должна считывать значения с клавиатуры.
+3. Программа должна содержать только три метода.
+4. Метод createArrayList() должен создавать и возвращать список ArrayList.
+5. Метод createLinkedList() должен создавать и возвращать список LinkedList.
+*/
+
+public class Solution {
+    public static Object createArrayList() {
+        return new ArrayList<>();
+
+    }
+
+    public static Object createLinkedList() {
+        return new LinkedList<>();
+
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
